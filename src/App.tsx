be415571f3232f0354ee5c98d652a5fe6a889948
ANDREW NGO <ango@udel.edu>
2023-04-29 12:28:38 -0400

    Added routing pages and sidebar links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Landing from "./pages/Landing";
+import Strategy from "./pages/Strategy";
+import Forum from "./pages/Forum";
 //Routing
 import { Routes, Route } from "react-router-dom";
 
@@ -8,6 +10,8 @@ export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Landing />}></Route>
+            <Route path="/Strategy" element={<Strategy />}></Route>
+            <Route path="/Forum" element={<Forum />}></Route>
         </Routes>
     );
 }
